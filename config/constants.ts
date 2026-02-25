d866115dab8ed35f25723bfcf3bf8dd1e0981eed
mahirusman <usmancuiisb@gmail.com>
2026-02-25 21:25:27 +0300

    refactor: remove legacy snake_case modules and normalize shared types

--- config/constants.ts
+++
@@ -1,6 +0,0 @@
-const { MONGOURL, JWT_SERCTET, JWT_EXPIRE_IN } = process.env;
-export = {
-  mongourl: MONGOURL || '',
-  jwt_secrit: JWT_SERCTET || '',
-  jwt_expire_in: JWT_EXPIRE_IN || '',
-};
--- types.d.ts
+++ types.d.ts
@@ -1,12 +1,5 @@
-type RequestBody =
-  | {
-      [key: string]: boolean | number | string | string[] | number[] | null;
-    }
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  | any;
+type RequestBody = {
+  [key: string]: boolean | number | string | string[] | number[] | null;
+};
 
-type AllowedParameters = Array<string>;
-
-type DecodeUser = {
-  _id: Pick<IUserDocument, "_id">;
-} | null;
+type AllowedParameters = string[];
